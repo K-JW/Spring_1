95f620afb81c7b003fd0b6b8324cfdb1e4344127
KimJinWoo <technical1500@gmail.com>
2024-07-04 22:10:09 +0900

    JWTUtil - JWT 생성 및 생성된 토큰을 통해 클레임 값을 반환하는 메소드 작성

---
+++ src/main/java/org/portfolio/spring_1/jwt/JWTUtil.java
@@ -0,0 +1,52 @@
+package org.portfolio.spring_1.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret-key}") String secret) {
+        secretKey = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm() // == HmacSHA256
+        );
+    }
+
+    // 클레임 값 반환 메소드
+    public Claims getClaimValue(String token) {
+        return Jwts.parser().verifyWith(secretKey).build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getClaim(String token, String claimName) {
+        return getClaimValue(token).get(claimName, String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return getClaimValue(token).getExpiration().before(new Date());
+    }
+
+    public String createJwt(String serial, String role, String category, String name, String provider, Long expiredMs) {
+        return Jwts.builder()
+                .claim("serial", serial)
+                .claim("role", role)
+                .claim("category", category)
+                .claim("name", name)
+                .claim("provider", provider)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
