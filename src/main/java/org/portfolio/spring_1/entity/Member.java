8f112d55da1512ce3e63dbf1856896409ae7eb3f
KimJinWoo <technical1500@gmail.com>
2024-07-06 23:53:19 +0900

    Member - Member Entity

---
+++ src/main/java/org/portfolio/spring_1/entity/Member.java
@@ -0,0 +1,28 @@
+package org.portfolio.spring_1.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Member {
+
+    @Id
+    @Column(name = "serial", unique = true)
+    private String serial;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "provider")
+    private String provider;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole memberRole;
+}
