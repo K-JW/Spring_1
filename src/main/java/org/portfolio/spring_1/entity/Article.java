82075431cb4a9d689cf720b860b497a4be2b43be
KimJinWoo <technical1500@gmail.com>
2024-07-14 23:55:38 +0900

    Article - Article Entity

---
+++ src/main/java/org/portfolio/spring_1/entity/Article.java
@@ -0,0 +1,41 @@
+package org.portfolio.spring_1.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "article")
+public class Article extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "name", referencedColumnName = "serial")
+    private Member author;
+
+    @Builder
+    public Article(Member author, String title, String content) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
