4c8ef18cd35987a064a247d58767bbeb1746ef43
KimJinWoo <technical1500@gmail.com>
2024-07-15 23:54:42 +0900

    MemberRepository - findBySerial method에 Query문 추가

--- src/main/java/org/portfolio/spring_1/repository/MemberRepository.java
+++ src/main/java/org/portfolio/spring_1/repository/MemberRepository.java
@@ -2,8 +2,11 @@ package org.portfolio.spring_1.repository;
 
 import org.portfolio.spring_1.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
-    Member findBySerial(String serial);
+    @Query("SELECT m FROM Member m WHERE m.serial = :serial")
+    Member findBySerial(@Param("serial") String serial);
 }
