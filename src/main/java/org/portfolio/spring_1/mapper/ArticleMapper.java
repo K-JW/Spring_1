f22bb5f0c37d84cd26c4b3e7633ceb0f445459df
KimJinWoo <technical1500@gmail.com>
2024-07-22 22:30:38 +0900

    ArticleMapper - dto를 entity로, entity를 dto로 변환하는 method 구현

---
+++ src/main/java/org/portfolio/spring_1/mapper/ArticleMapper.java
@@ -0,0 +1,38 @@
+package org.portfolio.spring_1.mapper;
+
+import org.portfolio.spring_1.dto.ArticleRequestDTO;
+import org.portfolio.spring_1.dto.ArticleResponseDTO;
+import org.portfolio.spring_1.entity.Article;
+import org.portfolio.spring_1.entity.Member;
+import org.springframework.stereotype.Component;
+
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class ArticleMapper {
+
+    public Article toEntity(ArticleRequestDTO articleRequestDTO, Member author) {
+        return Article.builder()
+                .title(articleRequestDTO.getTitle())
+                .content(articleRequestDTO.getContent())
+                .author(author)
+                .build();
+    }
+
+    public ArticleResponseDTO toDTO(Article article) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
+        String formattedCreatedAt = article.getCreatedAt().format(formatter);
+        String formattedModifiedAt = article.getModifiedAt().format(formatter);
+
+        return ArticleResponseDTO.builder()
+                .id(article.getId())
+                .title(article.getTitle())
+                .content(article.getContent())
+                .authorName(article.getAuthor().getName())
+                .createdAt(formattedCreatedAt)
+                .modifiedAt(formattedModifiedAt)
+                .viewCount(article.getViewCount())
+                .build();
+    }
+
+}
