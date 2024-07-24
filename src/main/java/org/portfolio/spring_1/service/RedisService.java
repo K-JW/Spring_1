3eef2f0a083af70bac538606ed01e34faef893c5
KimJinWoo <technical1500@gmail.com>
2024-07-24 22:50:05 +0900

    RedisService - JWT refresh token을 redis에 저장, redis에 token 존재 여부 조회, 삭제 method 정의

---
+++ src/main/java/org/portfolio/spring_1/service/RedisService.java
@@ -0,0 +1,40 @@
+package org.portfolio.spring_1.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    // Refresh Token TTL 설정
+    public void setValues(String key, String data, Duration duration) {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        values.set(key, data, duration);
+    }
+
+    public String getValues(String key) {
+        ValueOperations<String, Object> values = redisTemplate.opsForValue();
+        if (values.get(key) == null) {
+            return "false";
+        }
+        return (String) values.get(key);
+    }
+
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public boolean checkExistsValue(String value) {
+        return value.equals("false");
+    }
+
+}
