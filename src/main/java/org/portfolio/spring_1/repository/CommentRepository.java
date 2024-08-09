f3fcb3ab9672be1578a06144753de4ca029482d7
KimJinWoo <technical1500@gmail.com>
2024-08-09 22:28:35 +0900

    CommentRepository - 게시글 아이디로 해당 게시글의 전체 댓글 조회 메소드, 게시글 아이디로 해당 게시글의 전체 댓글 수 조회 메소드, 댓글 아이디로 부모 댓글 조회 메소드, 부모 댓글 아이디로 전체 자식 댓글 (답글) 조회 메소드 정의

--- src/main/java/org/portfolio/spring_1/repository/CommentRepository.java
+++ src/main/java/org/portfolio/spring_1/repository/CommentRepository.java
@@ -2,6 +2,22 @@ package org.portfolio.spring_1.repository;
 
 import org.portfolio.spring_1.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT cm FROM Comment cm WHERE cm.article.id = :articleId")
+    List<Comment> findAllByArticleId(@Param("articleId") Long articleId);
+
+    @Query("SELECT count(cm) FROM Comment cm WHERE cm.article.id = :articleId")
+    int findCommentCountByArticleId(@Param("articleId")Long articleId);
+
+    @Query("SELECT cm FROM Comment cm WHERE cm.id = :parentCommentId")
+    Comment findParentCommentById(@Param("parentCommentId") Long parentCommentId);
+
+    @Query("SELECT cm FROM Comment cm WHERE cm != :getComment And cm.parentComment = :getComment ORDER BY cm.id ASC")
+    List<Comment> findAllByParentComment(@Param("getComment")Comment getComment);
 }
