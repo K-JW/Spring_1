9852c794baea9d29d35a96798660264d701da83a
KimJinWoo <technical1500@gmail.com>
2024-08-14 21:17:00 +0900

    Spring1ApplicationTests - spring project test

---
+++ src/test/java/org/portfolio/spring_1/Spring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.portfolio.spring_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
