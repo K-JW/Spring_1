ab86bd98a070f506d8828c4ad116a6c0e75b5463
jin_____w <technical1500@gmail.com>
2024-08-27 23:45:22 +0900

    CommentResponseDTO - 멤버 이름 필드를 멤버 객체 필드로 변경

--- src/main/java/org/portfolio/spring_1/dto/CommentResponseDTO.java
+++ src/main/java/org/portfolio/spring_1/dto/CommentResponseDTO.java
@@ -3,6 +3,7 @@ package org.portfolio.spring_1.dto;
 import lombok.Builder;
 import lombok.Getter;
 import org.portfolio.spring_1.entity.Comment;
+import org.portfolio.spring_1.entity.Member;
 
 @Getter
 @Builder
@@ -12,7 +13,7 @@ public class CommentResponseDTO {
     private Long commentId;
     private Comment parentComment;
     private Comment replyComment;
-    private String authorName;
+    private Member authorMember;
     private String createdAt;
     private String modifiedAt;
     private String content;
