d870eb55a6642a5908128e881ba578082510fbe8
jin_____w <technical1500@gmail.com>
2024-08-31 23:39:55 +0900

    CommentRepository - findAllByArticleId과 동일한 name의 paging용 method 정의

--- src/main/java/org/portfolio/spring_1/repository/CommentRepository.java
+++ src/main/java/org/portfolio/spring_1/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package org.portfolio.spring_1.repository;
 
 import org.portfolio.spring_1.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +10,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("SELECT cm FROM Comment cm WHERE cm.article.id = :articleId")
+    Page<Comment> findAllByArticleId(@Param("articleId") Long articleId, @Param("pageable") Pageable pageable);
 
     @Query("SELECT cm FROM Comment cm WHERE cm.article.id = :articleId")
     List<Comment> findAllByArticleId(@Param("articleId") Long articleId);
