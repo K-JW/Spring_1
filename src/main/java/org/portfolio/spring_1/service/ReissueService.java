035f3130b9900f0acc07ee95d233ab12fb8cf9ed
jin_____w <technical1500@gmail.com>
2024-09-10 23:30:08 +0900

    ReissueService - JWTFilter에 구현된 checkRefreshToken method를 삭제후 ReissueService에 구현, 각 토큰의 저장 위치를 쿠키에서 변경 : accessToken = http request header / refreshToken = redis

--- src/main/java/org/portfolio/spring_1/service/ReissueService.java
+++ src/main/java/org/portfolio/spring_1/service/ReissueService.java
@@ -1,12 +1,12 @@
 package org.portfolio.spring_1.service;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.portfolio.spring_1.jwt.JWTFilter;
 import org.portfolio.spring_1.jwt.JWTUtil;
-import org.portfolio.spring_1.oauth2.CustomSuccessHandler;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -18,39 +18,36 @@ public class ReissueService {
 
     private static final long ACCESS_TOKEN_EXPIRATION = 600000L; // 10분
     private static final long REFRESH_TOKEN_EXPIRATION = 600000 * 6 * 24L; // 24시간
+    private static final int BAD_REQUEST = HttpServletResponse.SC_BAD_REQUEST; // == 400
+
 
     private final JWTUtil jwtUtil;
-    private final JWTFilter jwtFilter;
     private final RedisService redisService;
-    private final CustomSuccessHandler customSuccessHandler;
 
-    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response, String accessToken) {
 
-        String refresh = null;
+        String getSerial = jwtUtil.getClaim(accessToken, "serial");
 
-        Cookie[] cookies = request.getCookies();
+        String getRefresh = redisService.getValues(getSerial);
 
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("refresh")) {
-                refresh = cookie.getValue();
-            }
-        }
+        String checkedRefresh = checkRefreshToken(getRefresh, response);
 
-        String checkedRefresh = jwtFilter.checkRefreshToken(refresh, response);
-
-        String serial = jwtUtil.getClaim(refresh, "serial");
-        String name = jwtUtil.getClaim(refresh, "name");
-        String role = jwtUtil.getClaim(refresh, "role");
-        String provider = jwtUtil.getClaim(refresh, "provider");
-        String category = jwtUtil.getClaim(refresh, "category");
+        String serial = jwtUtil.getClaim(checkedRefresh, "serial");
+        String name = jwtUtil.getClaim(checkedRefresh, "name");
+        String role = jwtUtil.getClaim(checkedRefresh, "role");
+        String provider = jwtUtil.getClaim(checkedRefresh, "provider");
+        String category = jwtUtil.getClaim(checkedRefresh, "category");
 
         String newAccessToken = jwtUtil.createJwt("access", serial, role, name, provider, ACCESS_TOKEN_EXPIRATION);
         String newRefreshToken = jwtUtil.createJwt(category, serial, role, name, provider, REFRESH_TOKEN_EXPIRATION);
 
         redisService.setValues(serial, newRefreshToken, Duration.ofMillis(86400000L));
 
-        response.addCookie(createCookie("access", newAccessToken));
-        response.addCookie(createCookie(category, newRefreshToken));
+        response.setHeader("Authorization", "Bearer " + newAccessToken);
+//        response.addCookie(createCookie(category, newRefreshToken));
+//        response.addCookie(createCookie("access", newAccessToken));
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     private Cookie createCookie(String token, String value) {
@@ -71,6 +68,41 @@ public class ReissueService {
         tokenCookie.setHttpOnly(true);
 
         return tokenCookie;
+    }
+
+    public String checkRefreshToken(String token, HttpServletResponse response) {
+
+        // null 확인
+        if (token == null) {
+            response.setStatus(BAD_REQUEST);
+            return null;
+        }
+
+        // expiration 확인
+        try {
+            jwtUtil.isExpired(token);
+        } catch (ExpiredJwtException e) {
+            response.setStatus(BAD_REQUEST);
+            return null;
+        }
+
+        // category 확인
+        String category = jwtUtil.getClaim(token, "category");
+
+        if (!category.equals("refresh")) {
+            response.setStatus(BAD_REQUEST);
+            return null;
+        }
+
+        // redis에 존재 여부 확인
+        String key = jwtUtil.getClaim(token, "serial");
+
+        if (redisService.getValues(key) == null) {
+            response.setStatus(BAD_REQUEST);
+            return null;
+        }
+
+        return key;
     }
 
 }
